74fb128ec299f78b921487078a40a42571dea335
Jose <jdelsol20@gmail.com>
2023-05-31 13:54:55 -0700

    Select a Featured Puppy tab

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,33 @@ import './App.css';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+
+  console.log("Featured Puppy:", featuredPup);
+
   return (
     <>
-      <div>
+      <div className="App">
         {puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
+     return <p onClick={() => {setFeatPupId(puppy.id);
+    }} 
+    key={puppy.id}>{puppy.name}</p>;
    })}
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   );
